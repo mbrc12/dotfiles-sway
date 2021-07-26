875214212788e76b0b96d291c22f24411e4fb86b
Mriganka Basu Roy Chowdhury <mbrc12@gmail.com>
2021-07-26 13:04:34 +0530

    Increase st font size

--- st/config.h
+++ st/config.h
@@ -5,7 +5,7 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "Fira Mono:pixelsize=21:antialias=true:autohint=true";
+static char *font = "Fira Mono:pixelsize=23:antialias=true:autohint=true";
 
 static char *font2[] = {
     "Fira Mono:pixelsize=21:antialias=true:autohint=true",
