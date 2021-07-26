ce04683e0a99f03027a413146a2d9c030dc7e6ab
Mriganka Basu Roy Chowdhury <mbrc12@gmail.com>
2021-07-26 20:41:27 +0530

    Modified fish

--- fish/config.fish
+++ fish/config.fish
@@ -6,6 +6,6 @@ if status is-login
 	exec runsway
 end
 
-set -Ux SWAYSOCK /run/user/(id -u)/sway-ipc.(id -u).(pgrep -x sway).sock
+#set -Ux SWAYSOCK /run/user/(id -u)/sway-ipc.(id -u).(pgrep -x sway).sock
 set -Ux SSDIR $HOME/media/ss
 
